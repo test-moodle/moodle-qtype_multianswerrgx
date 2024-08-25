a0f6cfe8a4bcbde745ed0bff9298051c62f18c9a
rezeau <joseph@rezeau.org>
2024-08-25 22:54:27 +0200

    version 2024082500 adds listing of developed and alternative correct answers in the edit form
    Signed-off-by: rezeau <joseph@rezeau.org>

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_multianswerrgx';
-$plugin->version   = 2024081900;
+$plugin->version   = 2024082500;
 
 $plugin->requires  = 2024041600;
 $plugin->dependencies = [
