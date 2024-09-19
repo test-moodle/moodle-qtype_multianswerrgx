953f2b0cf354c7f1e65ec62fc0680986d6ae0fe9
rezeau <joseph@rezeau.org>
2024-09-19 18:24:03 +0200

    added French translations
    Signed-off-by: rezeau <joseph@rezeau.org>

---
+++ lang/fr/qtype_multianswerrgx.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_multianswerrgx', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    qtype_multianswerrgx
+ * @subpackage multianswerrgx
+ * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addclozegaps5'] = '1/5 blancs';
+$string['addclozegaps9'] = '1/9 blancs';
+$string['addgaps_help'] = 'Activez la fonction « Ajouter des blancs » pour créer automatiquement des blancs de type SHORTANSWER dans le texte de la question, soit tous les 5 mots, soit tous les 9 mots.';
+$string['addgapserror'] = 'Ce texte de question comporte déjà des blancs';
+$string['addgapslabel'] = 'Ajouter des blancs';
+$string['addgapslabel_help'] = 'Ajoute automatiquement des blancs au texte de la question, soit tous les 5 mots, soit tous les 9 mots. N\'utilise que le type de question SHORTANSWER. Remarque : le bouton « Supprimer toutes les lacunes » supprime toutes les sous-questions existantes du texte de la question et réactive les boutons « Ajouter des lacunes ».';
+$string['removegaps'] = 'Supprimer tous les blancs';
+$string['skipcapswords'] = 'Ne pas supprimer les noms propres';
