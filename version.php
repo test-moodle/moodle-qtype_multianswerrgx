799387e1d0253af57edaf6b85f4ebe0b02e80886
rezeau <joseph@rezeau.org>
2024-09-23 19:10:22 +0200

    version 2024092300; added atto check and changed string
    Signed-off-by: rezeau <joseph@rezeau.org>

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_multianswerrgx';
-$plugin->version   = 2024092200;
+$plugin->version   = 2024092300;
 
 $plugin->requires  = 2024041600;
 $plugin->dependencies = [
